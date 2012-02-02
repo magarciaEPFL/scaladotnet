27ffaa1b04e3234ebb1ccba62d592c1118e412c8
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2012-02-02 14:41:11 +0100

    sync with scala/scala@3aebe255b87f534239f0c46a2a6e0d696c8a31d4

--- src/compiler/scala/tools/nsc/interpreter/AbstractFileClassLoader.scala
+++ src/compiler/scala/tools/nsc/interpreter/AbstractFileClassLoader.scala
@@ -20,6 +20,7 @@ class AbstractFileClassLoader(root: AbstractFile, parent: java.lang.ClassLoader)
     with ScalaClassLoader
 {
   // private val defined = mutable.Map[String, Class[_]]()
+
   override protected def trace =
     sys.props contains "scala.debug.classloader"
 
@@ -41,6 +42,22 @@ class AbstractFileClassLoader(root: AbstractFile, parent: java.lang.ClassLoader)
       case null   => null
       case file   => file
     }
+  }
+
+  protected def dirNameToPath(name: String): String =
+    _root_.java.lang.String.instancehelper_replace(name, '.', '/')
+
+  protected def findAbstractDir(name: String): AbstractFile = {
+    var file: AbstractFile = root
+    val pathParts          = dirNameToPath(name) split '/'
+
+    for (dirPart <- pathParts) {
+      file = file.lookupName(dirPart, true)
+      if (file == null)
+        return null
+    }
+
+    return file
   }
 
   override def getResourceAsStream(name: String) = findAbstractFile(name) match {
@@ -74,4 +91,24 @@ class AbstractFileClassLoader(root: AbstractFile, parent: java.lang.ClassLoader)
   //   case null   => super.getResource(name)
   //   case file   => new URL(...)
   // }
+
+  private val packages = mutable.Map[String, java.lang.Package]()
+
+  override def definePackage(name: String, specTitle: String, specVersion: String, specVendor: String, implTitle: String, implVersion: String, implVendor: String, sealBase: URL): java.lang.Package = {
+    throw new java.lang.UnsupportedOperationException()
+  }
+
+  override def getPackage(name: String): java.lang.Package = {
+    findAbstractDir(name) match {
+      case null => super.getPackage(name)
+      case file => packages.getOrElseUpdate(name, {
+        val ctor = classOf[java.lang.Package].getDeclaredConstructor(scala.Array( classOf[String], classOf[String], classOf[String], classOf[String], classOf[String], classOf[String], classOf[String], classOf[URL], classOf[java.lang.ClassLoader] ) )
+        ctor.setAccessible(true)
+        ctor.newInstance(scala.Array( name, null, null, null, null, null, null, null, this ) ).asInstanceOf[java.lang.Package]
+      })
+    }
+  }
+
+  override def getPackages(): Array[java.lang.Package] =
+    root.iterator.filter(_.isDirectory).map(dir => getPackage(dir.name)).toArray
 }
